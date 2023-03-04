67bfea3d5bc2d69d6178ecd9e32a08463c7dcd1c
Gajendra <raogajendra27@gmail.com>
2023-03-04 14:44:50 +0530

    object, FormData

--- main.js
+++ main.js
@@ -15,9 +15,26 @@ formEl.addEventListener('submit', function(e){
     e.preventDefault()
     txt.innerHTML = `<div class="modal-inner-loading">
     <img src="loading.svg" class="loading">
-    <p id="uploadText">
+    <p id="upload-text">
         Uploading your data to the dark web...
     </p>
 </div>`
+setTimeout(function(){
+    document.getElementById('upload-text').innerText = `Making the sale...`
+}, 1500) 
+
+setTimeout(function(){
+    document.getElementById('modal-inner').innerHTML = `
+    <h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="pirate.gif">
+    </div>
+` 
+}, 3000)
+
+const formData = new FormData(formEl)
+    console.log(formEl);
 })
+
 
